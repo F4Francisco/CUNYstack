cbe81d9c4d116ee68f257850e5c867e2cb33763e
Francisco Perez <Francisco.Perez4@lc.cuny.edu>
2016-11-01 19:23:23 -0400

    Uploading Entire Project

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'CUNYStack'
+    });
+});
+
+router.post('/results', function(req, res) {
+    res.render('results', {
+        school: req.body.school,
+        subject: req.body.subject,
+        courseID: req.body.courseID
+    });
+});
+
+router.post('/detail', function(req,res,next){
+    res.render('detail', {});
+});
+
+module.exports = router;
